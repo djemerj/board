67f988b4e755f01f3a3963d842b406c08a98c144
jgilbert <jeremy.gilbert@docapost.fr>
2020-02-10 21:47:52 +0100

    Bug correction

--- src/Board.jsx
+++ src/Board.jsx
@@ -30,7 +30,7 @@ class Board extends Component {
      * @param index
      */
     onDragStart = (e, index) => {
-        this.draggedItem = this.state.squares.find(element => element.props.id === index);
+        this.draggedIndex = this.state.squares.findIndex(element => element.props.id === index);
 
         e.dataTransfer.effectAllowed = "move";
         e.dataTransfer.setData("text/html", e.target.parentNode);
@@ -38,19 +38,19 @@ class Board extends Component {
     };
 
     /**
-     * Drag over event
+     * Drop event, invert the squares
      * @param index
      */
-    onDragOver = index => {
-        const draggedOverItem = this.state.squares.find(element => element.props.id === index);
+    drop = (e, index) => {
+        const draggedOverIndex = this.state.squares.findIndex(element => element.props.id === index);
 
-        if (this.draggedItem === draggedOverItem) {
-            return;
-        }
+        let origin = this.state.squares[this.draggedIndex];
+        let target = this.state.squares[draggedOverIndex];
 
-        let squares = this.state.squares.filter(square => square !== this.draggedItem);
+        let squares = this.state.squares;
 
-        squares.splice(index, 0, this.draggedItem);
+        squares[draggedOverIndex] = origin;
+        squares[this.draggedIndex] = target;
 
         this.setState({squares});
     };
@@ -59,7 +59,7 @@ class Board extends Component {
      * Drag end event
      */
     onDragEnd = () => {
-        this.draggedItem = null;
+        this.draggedIndex = null;
     };
 
     /**
@@ -72,19 +72,22 @@ class Board extends Component {
         const y = Math.floor(i / 4);
         const black = (x + y) % 2 === 1;
         const backgroundColor = black ? 'black' : 'white';
+        const color = black ? 'white' : 'black';
         const item = 'item' + i;
 
         return (
-            <div key={item} style={squareStyle} id={i} onDragOver={() => this.onDragOver(i)}>
+            <div key={item} style={squareStyle} id={i} onDragOver={(e) => e.preventDefault()}>
                 <div style={{
                     position: 'relative',
                     width: '100%',
                     height: '100%',
                     cursor: 'grab',
                     backgroundColor,
+                    color,
                 }}
                      draggable
                      onDragStart={e => this.onDragStart(e, i)}
+                     onDrop={e => this.drop(e, i)}
                      onDragEnd={this.onDragEnd}>
                     {i}
                 </div>
